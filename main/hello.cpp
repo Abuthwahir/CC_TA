36c4d895e235a5ae676407aa6701e13c3cc5745e
Abuthwahir <144421565+Abuthwahir@users.noreply.github.com>
2025-03-13 16:24:26 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,9 +3,10 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
+  
   cout << "Hello, Jenkins\n";
-  cout << "Hello, PES1UG22CS022\n"; 
+  cout << "Hello, PES1UG22CS022\n";
+  
   cout << "I have successfully built and run this  \n";
   return 0;
 }
